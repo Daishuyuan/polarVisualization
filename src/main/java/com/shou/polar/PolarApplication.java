18867db6d0443256cdee0c2bc927158fb74e9890
polar <2250649759@qq.com>
2018-10-19 18:10:08 +0800

    前端代码移植

--- src/main/java/com/shou/polar/PolarApplication.java
+++ src/main/java/com/shou/polar/PolarApplication.java
@@ -4,10 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class PolarApplication {
+
+    @RequestMapping("/")
+    public String startPage() {
+        return "index";
+    }
+
+    @RequestMapping("/cutScene")
+    public String cutScene() {
+        return "cutScene";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PolarApplication.class, args);

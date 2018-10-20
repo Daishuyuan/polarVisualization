06a06286dc64d7cf1a8f3c5d9c21b1b41f575e99
polar <2250649759@qq.com>
2018-10-20 18:06:10 +0800

    新增资源文件夹

--- src/main/java/com/shou/polar/controller/ThemeResController.java
+++ src/main/java/com/shou/polar/controller/ThemeResController.java
@@ -1,5 +1,7 @@
 package com.shou.polar.controller;
 
+import com.shou.polar.service.UpdateEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class ThemeResController {
+    private UpdateEvent updateEvent;
+
+    @Autowired
+    public ThemeResController(UpdateEvent updateEvent) {
+        this.updateEvent = updateEvent;
+    }
+
     @RequestMapping("/theme/{name}")
     public String getThemeConfiguration(@PathVariable String name) {
 

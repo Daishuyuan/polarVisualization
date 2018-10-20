3cbcbe5650c163c96c57cee96b0b89c1c6b0e355
polar <2250649759@qq.com>
2018-10-20 17:40:30 +0800

    更改资源更新同步模块bug

--- src/main/java/com/shou/polar/controller/StaticResController.java
+++ src/main/java/com/shou/polar/controller/StaticResController.java
@@ -1,15 +1,18 @@
 package com.shou.polar.controller;
 
+import com.google.gson.JsonObject;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
-public class ThemeDataController {
-    @RequestMapping("/theme/{name}")
-    public String getThemeConfiguration(@PathVariable String name) {
+@RequestMapping("/staticRes")
+public class StaticResController {
 
-        return "";
+    @RequestMapping("/theme/{name}")
+    public JsonObject getThemePropsByName(@PathVariable String name) {
+
+        return null;
     }
+
 }
---
+++ src/main/java/com/shou/polar/controller/ThemeResController.java
@@ -0,0 +1,15 @@
+package com.shou.polar.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ThemeResController {
+    @RequestMapping("/theme/{name}")
+    public String getThemeConfiguration(@PathVariable String name) {
+
+        return "";
+    }
+}

7e03ec8968a0c891380d4d8b80cb92d0f7c3e62c
polar <2250649759@qq.com>
2018-10-22 18:16:14 +0800

    图表配置项目更新

--- src/main/java/com/shou/polar/controller/AllTestController.java
+++ src/main/java/com/shou/polar/controller/AllTestController.java
@@ -1,14 +1,28 @@
 package com.shou.polar.controller;
 
+import com.shou.polar.service.UpdateEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
 public class AllTestController {
+    private final UpdateEvent updateEvent;
+
+    @Autowired
+    public AllTestController(UpdateEvent updateEvent) {
+        this.updateEvent = updateEvent;
+    }
 
     @RequestMapping("/advice")
     public String testAdvice() {
         throw new RuntimeException("advice test");
+    }
+
+    @RequestMapping("/pushData/{name}")
+    public void pushData(@PathVariable String name) {
+        updateEvent.updateRes(name);
     }
 }
--- src/main/java/com/shou/polar/controller/ApiController.java
+++ src/main/java/com/shou/polar/controller/ApiController.java
@@ -1,6 +1,5 @@
 package com.shou.polar.controller;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.google.gson.Gson;
 import com.shou.polar.service.BackToFrontAdvice;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
@@ -10,7 +9,6 @@ import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
-import java.nio.charset.Charset;
 import java.util.List;
 
 @RestController
@@ -18,9 +16,12 @@ import java.util.List;
 public class ApiController {
     private final BackToFrontAdvice advice;
     private final Gson gson;
+    private static final String SPLIT = "/";
+    private static final String SUFFIX = ".json";
     private static final String DEFAULT_FILE_TYPE = "UTF-8";
     private static final String LOCAL_RES_PATH = "classpath:local";
     private static final String LOCAL_SCENES_RES_PATH = LOCAL_RES_PATH + "/scenes";
+    private static final String LOCAL_DIAGRAMS_RES_PATH = LOCAL_RES_PATH + "/diagrams";
 
     @Autowired
     public ApiController(BackToFrontAdvice advice) {
@@ -28,14 +29,8 @@ public class ApiController {
         gson = new Gson();
     }
 
-    @GetMapping("/errors")
-    public List<String> getErrors(HttpServletRequest request) {
-        return advice.getErrors(request.getSession().getId());
-    }
-
-    @RequestMapping("/scenes/{name}")
-    public String getThemePropsByName(@PathVariable String name) throws IOException {
-        File file = ResourceUtils.getFile(LOCAL_SCENES_RES_PATH + "/" + name + ".json");
+    private String readFileContentByPath(String path) throws IOException {
+        File file = ResourceUtils.getFile(path);
         BufferedReader reader = null;
         String res = "";
         if (file.exists()) {
@@ -46,6 +41,21 @@ public class ApiController {
             reader.close();
         }
         return res;
+    }
+
+    @GetMapping("/errors")
+    public List<String> getErrors(HttpServletRequest request) {
+        return advice.getErrors(request.getSession().getId());
+    }
+
+    @RequestMapping("/diagrams/{name}")
+    public String getDiagramsByName(@PathVariable String name) throws IOException {
+        return readFileContentByPath(LOCAL_DIAGRAMS_RES_PATH + SPLIT + name + SUFFIX);
+    }
+
+    @RequestMapping("/scenes/{name}")
+    public String getThemePropsByName(@PathVariable String name) throws IOException {
+        return readFileContentByPath(LOCAL_SCENES_RES_PATH + SPLIT + name + SUFFIX);
     }
 
 }

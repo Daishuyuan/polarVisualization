b3886507d21e5ee43324889122e9746e309e3a45
polar <2250649759@qq.com>
2018-11-08 13:15:12 +0800

    更改数据处理器的配置方式。

--- src/main/java/com/shou/polar/service/TargetPathScheduledTaskService.java
+++ src/main/java/com/shou/polar/service/TargetPathScheduledTaskService.java
@@ -3,6 +3,7 @@ package com.shou.polar.service;
 import com.google.gson.Gson;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.shou.polar.configure.PolarCts;
 import com.shou.polar.pojo.ConfigEntity;
 import com.shou.polar.pojo.DataProcessor;
@@ -16,11 +17,13 @@ import org.springframework.util.ResourceUtils;
 import org.springframework.util.StringUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Service
 public class TargetPathScheduledTaskService {
@@ -28,15 +31,16 @@ public class TargetPathScheduledTaskService {
     private static Map<String, DataProcessor> PROCESSORS;
     private static Logger logger = Logger.getLogger(TargetPathScheduledTaskService.class);
     private static Gson GSON = null;
-    private JsonArray configs = null;
+    private AtomicBoolean isInit;
 
     @Autowired
     public TargetPathScheduledTaskService() {
         GSON = new Gson();
+        isInit = new AtomicBoolean(false);
         PROCESSORS = Collections.synchronizedMap(new HashMap<>());
     }
 
-    private void saveConfig(File configFile) throws IOException {
+    private void saveConfig(final File configFile, final JsonElement configs) throws IOException {
         FileUtils.writeStringToFile(configFile, configs.toString(), StandardCharsets.UTF_8); // 写回配置
     }
 
@@ -56,8 +60,8 @@ public class TargetPathScheduledTaskService {
                 }
                 configs.set(i, GSON.toJsonTree(configEntity, ConfigEntity.class));
             }
-            this.configs = configs;
-            saveConfig(configFile);
+            saveConfig(configFile, configs);
+            isInit.set(true);
         } catch (IOException |
                 InstantiationException |
                 NoSuchMethodException |
@@ -76,27 +80,30 @@ public class TargetPathScheduledTaskService {
 
     @Scheduled(fixedRate = PolarCts.SCHEDULE_CYCLE)
     public void processDataByCycle() {
-        if (null != this.configs) {
-            long wakeTime = new Date().getTime(); // 获取当前时间
-            for (int i=0; i < this.configs.size(); ++i) {
-                ConfigEntity configEntity = GSON.fromJson(this.configs.get(i), ConfigEntity.class);
-                long UDCTime = Long.parseLong(configEntity.getCycleTimeRecord()); // 上一次更新日期
-                long UDC = configEntity.getScheduledCycle(); // 更新间隔
-                if (UDCTime - wakeTime >= UDC) { // 间隔时间大于更新间隔
-                    long nextTime = UDCTime + UDC;
-                    configEntity.setCycleTimeRecord(String.valueOf(nextTime)); // 刷新更新日期
-                    DataProcessor processor = PROCESSORS.get(configEntity.getName());
-                    executeProcess(processor); // 异步执行
-                    ConfigEntity entity = processor.getConfigEntity();
-                    logger.info(entity.getName() + ", " + entity.getDescription() + ", " + SDF.format(new Date(wakeTime)));
+        try {
+            if (isInit.get()) {
+                File configFile = ResourceUtils.getFile(PolarCts.UPDATE_DATA_CONFIG_FILE_PATH);
+                String configStr = FileUtils.readFileToString(configFile, StandardCharsets.UTF_8); // 读取文件内容
+                JsonArray configs = GSON.fromJson(configStr, JsonArray.class); // 转换成Json数组格式
+                long wakeTime = new Date().getTime(); // 获取当前时间
+                for (int i=0; i < configs.size(); ++i) {
+                    ConfigEntity configEntity = GSON.fromJson(configs.get(i), ConfigEntity.class);
+                    long UDCTime = Long.parseLong(configEntity.getCycleTimeRecord()); // 上一次更新日期
+                    long UDC = configEntity.getScheduledCycle(); // 更新间隔
+                    if (UDCTime - wakeTime >= UDC) { // 间隔时间大于更新间隔
+                        long nextTime = UDCTime + UDC;
+                        configEntity.setCycleTimeRecord(String.valueOf(nextTime)); // 刷新更新日期
+                        DataProcessor processor = PROCESSORS.get(configEntity.getName());
+                        executeProcess(processor); // 异步执行
+                        ConfigEntity entity = processor.getConfigEntity();
+                        logger.info(entity.getDescription() + "," + SDF.format(new Date(wakeTime)));
+                    }
+                    configs.set(i, GSON.toJsonTree(configEntity, ConfigEntity.class));
                 }
-                this.configs.set(i, GSON.toJsonTree(configEntity, ConfigEntity.class));
+                saveConfig(configFile, configs);
             }
-            try {
-                saveConfig(ResourceUtils.getFile(PolarCts.UPDATE_DATA_CONFIG_FILE_PATH));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
